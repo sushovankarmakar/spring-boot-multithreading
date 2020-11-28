7f81f7078705ca55c5edeee203b845c38b8c4cac
sushovankarmakar <officialkarmakar@gmail.com>
2020-11-28 08:52:34 +0530

    initial commit

---
+++ src/test/java/com/example/springbootmultithreading/SpringBootMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
